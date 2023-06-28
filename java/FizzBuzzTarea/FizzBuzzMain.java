236ede6443f648f8a73e774cb11fb847fb5c32b8
jorge <jorgeivandts@gmail.com>
2023-06-28 13:43:55 -0400

    fizzbuzz act

---
+++ java/FizzBuzzTarea/FizzBuzzMain.java
@@ -0,0 +1,15 @@
+package FizzBuzzTarea;
+
+public class FizzBuzzMain {
+
+	public static void main(String[] args) {
+		FizzBuzz fizzBuzz = new FizzBuzz();
+
+		System.out.println(fizzBuzz.fizzBuzz(15));
+		System.out.println(fizzBuzz.fizzBuzz(3));
+		System.out.println(fizzBuzz.fizzBuzz(5));
+		System.out.println(fizzBuzz.fizzBuzz(2));
+
+	}
+
+}
